22e3a89db74a4c6a01bae3282c2757ca0515809e
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-01-29 16:29:40 -0300

    banco aws

--- src/main/java/com/lucasdavi/quizz/configs/WebConfig.java
+++ src/main/java/com/lucasdavi/quizz/configs/WebConfig.java
@@ -1,6 +1,5 @@
 package com.lucasdavi.quizz.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,17 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500") // URL do seu frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://lucasdavi99.github.io") // URL do seu frontend
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
