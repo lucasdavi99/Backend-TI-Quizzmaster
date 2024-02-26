92c5a69e91577c8c13d7e18d6a554bb0e2baff7c
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-02-25 22:11:52 -0300

    consertando CORS

--- src/main/java/com/lucasdavi/quizz/configs/WebConfig.java
+++ src/main/java/com/lucasdavi/quizz/configs/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://lucasdavi.tech/")
+                .allowedOrigins("https://lucasdavi.tech")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
