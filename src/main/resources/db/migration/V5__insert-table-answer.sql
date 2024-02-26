09c35703807090e96459d009da28457db4cec7b0
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-02-26 13:57:14 -0300

    Alterando base de dados

---
+++ src/main/resources/db/migration/V5__insert-table-answer.sql
@@ -0,0 +1,139 @@
+-- Respostas para a pergunta 1
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('FTP', true, 1),
+('HTTP', false, 1),
+('SMTP', false, 1),
+('TCP/IP', false, 1);
+
+-- Respostas para a pergunta 2
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Python', false, 2),
+('Java', false, 2),
+('PHP', false, 2),
+('JavaScript', true, 2);
+
+-- Respostas para a pergunta 3
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Uniform Resource Locator', true, 3),
+('Universal Resource Locator', false, 3),
+('Unified Resource Locator', false, 3),
+('Unique Resource Locator', false, 3);
+
+-- Respostas para a pergunta 4
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Criar uma cópia local de um repositório remoto', true, 4),
+('Enviar alterações para o repositório remoto', false, 4),
+('Mesclar alterações de branches', false, 4),
+('Desfazer as últimas alterações no código', false, 4);
+
+-- Respostas para a pergunta 5
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Markup Language utilizado para estruturar o conteúdo de uma página web', true, 5),
+('Linguagem de programação para desenvolvimento web', false, 5),
+('Ferramenta de edição de imagens', false, 5),
+('Protocolo de comunicação entre clientes e servidores', false, 5);
+
+-- Respostas para a pergunta 6
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Um tipo de estrutura de controle que repete uma série de instruções enquanto uma condição é verdadeira', true, 6),
+('Um operador utilizado para realizar cálculos matemáticos', false, 6),
+('Um método de ordenação de dados em um array', false, 6),
+('Uma função utilizada para comparar valores em um loop', false, 6);
+
+-- Respostas para a pergunta 7
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('HTTPS adiciona uma camada de segurança através do protocolo SSL/TLS, enquanto o HTTP não oferece essa camada de segurança', true, 7),
+('HTTPS é mais rápido que o HTTP devido à compressão de dados', false, 7),
+('HTTP é utilizado apenas para transferência de arquivos estáticos', false, 7),
+('HTTPS é mais utilizado em servidores locais do que em servidores remotos', false, 7);
+
+-- Respostas para a pergunta 8
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Um conjunto de passos finitos para resolver um problema ou realizar uma tarefa', true, 8),
+('Uma expressão matemática complexa', false, 8),
+('Um tipo de dado utilizado em linguagens de programação', false, 8),
+('Um método de organização de dados em uma estrutura de dados', false, 8);
+
+-- Respostas para a pergunta 9
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Enviar as alterações locais para o repositório remoto', true, 9),
+('Atualizar o repositório local com as alterações do repositório remoto', false, 9),
+('Sincronizar diferentes branches de um repositório', false, 9),
+('Criar uma cópia local de um repositório remoto', false, 9);
+
+-- Respostas para a pergunta 10
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Um paradigma de programação baseado em objetos e classes', true, 10),
+('Um método de programação que utiliza estruturas condicionais', false, 10),
+('Uma técnica de programação que utiliza funções recursivas', false, 10),
+('Um modelo de programação voltado para o desenvolvimento de jogos', false, 10);
+
+-- Respostas para a pergunta 11
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Estilizar e formatar a aparência de uma página web', true, 11),
+('Controlar a lógica e o comportamento da página', false, 11),
+('Gerar dinamicamente o conteúdo da página', false, 11),
+('Interagir com o banco de dados', false, 11);
+
+-- Respostas para a pergunta 12
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Um dispositivo de segurança que monitora e controla o tráfego de rede', true, 12),
+('Um software utilizado para proteger o computador contra vírus', false, 12),
+('Um aplicativo de segurança utilizado para criptografar dados', false, 12),
+('Um protocolo de comunicação utilizado para transferência de arquivos', false, 12);
+
+-- Respostas para a pergunta 13
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Linguagens de alto nível são mais abstratas e próximas da linguagem humana, enquanto linguagens de baixo nível são mais próximas da linguagem de máquina e mais difíceis de entender', true, 13),
+('Linguagens de alto nível são mais rápidas que linguagens de baixo nível devido à otimização de código', false, 13),
+('Linguagens de alto nível são exclusivamente utilizadas em sistemas embarcados', false, 13),
+('Linguagens de baixo nível são mais fáceis de aprender para iniciantes', false, 13);
+
+-- Respostas para a pergunta 14
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Um software que armazena e distribui páginas web para os clientes que as solicitam', true, 14),
+('Um hardware responsável pelo processamento de dados em uma rede', false, 14),
+('Um protocolo de comunicação utilizado para transferência de arquivos', false, 14),
+('Um sistema operacional utilizado em servidores de hospedagem', false, 14);
+
+-- Respostas para a pergunta 15
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Um espaço de memória reservado para armazenar valores que podem variar durante a execução do programa', true, 15),
+('Um tipo de dado que armazena valores fixos e imutáveis', false, 15),
+('Um tipo de estrutura de controle que repete uma série de instruções', false, 15),
+('Um método de programação utilizado para organizar e estruturar o código', false, 15);
+
+-- Respostas para a pergunta 16
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Um sistema que registra e controla as alterações feitas nos arquivos ao longo do tempo, permitindo que você reverta para versões específicas mais tarde', true, 16),
+('Um software de backup utilizado para armazenar cópias de segurança dos arquivos', false, 16),
+('Um protocolo de comunicação utilizado para sincronizar arquivos entre dispositivos', false, 16),
+('Um sistema de criptografia utilizado para proteger arquivos confidenciais', false, 16);
+
+-- Respostas para a pergunta 17
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('JavaScript é uma linguagem de script para páginas web, enquanto Java é uma linguagem de programação de propósito geral', true, 17),
+('Java é utilizado exclusivamente para o desenvolvimento de aplicativos móveis', false, 17),
+('JavaScript é uma versão mais recente do Java', false, 17),
+('Java é uma biblioteca de funções utilizada em páginas web', false, 17);
+
+-- Respostas para a pergunta 18
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Domain Name System (Sistema de Nomes de Domínio) é um sistema que traduz nomes de domínio legíveis para humanos em endereços IP numéricos', true, 18),
+('Dynamic Network System (Sistema de Rede Dinâmica) é um protocolo utilizado para alocar endereços IP dinamicamente', false, 18),
+('Data Network Security (Segurança de Rede de Dados) é um conjunto de medidas para proteger a integridade dos dados em uma rede', false, 18),
+('Database Naming Service (Serviço de Nomenclatura de Banco de Dados) é um sistema de gerenciamento de dados utilizado para nomear tabelas e colunas em um banco de dados', false, 18);
+
+-- Respostas para a pergunta 19
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('Linguagens interpretadas são executadas linha por linha, enquanto linguagens compiladas são traduzidas para código de máquina antes da execução', true, 19),
+('Linguagens interpretadas são mais rápidas que linguagens compiladas devido à interpretação direta do código fonte', false, 19),
+('Linguagens compiladas são mais flexíveis que linguagens interpretadas devido à execução dinâmica do código', false, 19),
+('Linguagens interpretadas são mais difíceis de depurar que linguagens compiladas', false, 19);
+
+-- Respostas para a pergunta 20
+INSERT INTO answer (content, is_correct, question_id) VALUES
+('SQL (Structured Query Language) é uma linguagem de programação utilizada para gerenciar e manipular bancos de dados relacionais', true, 20),
+('SQL é uma linguagem de programação de propósito geral', false, 20),
+('SQL é exclusivamente utilizado para desenvolvimento web', false, 20),
+('SQL é uma linguagem de programação interpretada', false, 20);
