c5f3b329aefa1a763b9b57f50adc56a7cd4e31ae
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-02-25 21:03:14 -0300

    Merge da branch develop para atualizar a branch main

--- src/main/java/com/lucasdavi/quizz/configs/WebConfig.java
+++ src/main/java/com/lucasdavi/quizz/configs/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500") // URL do seu frontend
+                .allowedOrigins("https://lucasdavi.tech/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
