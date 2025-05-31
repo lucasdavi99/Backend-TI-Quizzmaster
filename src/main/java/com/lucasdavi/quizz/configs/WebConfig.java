f3b9b992d6ba8c374ba2b6cb735d7b53730e20dd
Lucas Davi <lucasdavi99@hotmail.com>
2025-05-31 14:27:38 -0300

    refactor: mudar cors

--- src/main/java/com/lucasdavi/quizz/configs/WebConfig.java
+++ src/main/java/com/lucasdavi/quizz/configs/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://lucasdavi.tech")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("https://lucasdavi.tech", "http://localhost:3000", "http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
